28493d5bbaa6ff53f49749007ab6e627c1ac5b6e
Grzegorz Kaszuba <uirapuru@tlen.pl>
2013-10-18 02:10:36 +0200

    update

---
+++ app/config/deploy.rb
@@ -0,0 +1,33 @@
+set   :application,   "Camangula.com"
+set   :deploy_to,     "/home/uirapuru/domains/camangula.com/"
+set   :domain,        "s78.vdl.pl"
+
+set   :scm,           :git
+# set   :scm_passphrase, "E==m*c^2"
+set   :port,          "59184"
+set   :repository,    "ssh://s78.vdl.pl:59184/home/uirapuru/domains/camangula.git"
+
+ssh_options[:port] = "59184"
+set :ssh_options, { :forward_agent => true }
+
+role  :web,           "s78.vdl.pl"
+role  :app,           "s78.vdl.pl", :primary => true
+
+set   :use_sudo,      false
+set   :keep_releases, 3
+
+set :app_path,    "app"
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
+
+namespace :deploy do
+  task :composer, :roles => :web do
+    run "cd #{ current_path } && ./composer install -q"
+  end
+end
+
+after "deploy", "deploy:composer" 
